6f07005cbfadbd2622dac9b7d0d9b9b1feee67d5
christierbt <98954266+christierbt@users.noreply.github.com>
2022-06-29 18:19:48 +0800

    Update Footer.js

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <span class="footer">
+      <a href="https://github.com/christierbt/weatherapp_react">
+        Open-source code
+      </a>{" "}
+      by Christie Rosenblatt
+    </span>
+  );
+}
